404c32a728c45ec47cfc7c809dffc6d15efcd296
Francesco Limpias <francescolimpias@gmail.com>
2022-11-11 10:12:09 +0100

    Added dices bonus

--- dadi/main.js
+++ dadi/main.js
@@ -1,15 +1,32 @@
-// Throw dices
-let userThrow = 1 + parseInt(Math.random() * 5);
-let computerThrow = 1 + parseInt(Math.random() * 5);
+// References
+const throwComp = document.getElementById("throw-comp");
+const throwUser = document.getElementById("throw-user");
+const buttonGen = document.getElementById("gen");
+const output = document.getElementById("output");
 
-// Log throws
-console.log(`Hai lanciato ${userThrow}, il computer ha lanciato ${computerThrow}`);
+buttonGen.addEventListener("click", function () {
 
-// Check for result
-if (userThrow == computerThrow) {
-    console.log("Pareggio");
-} else if(userThrow < computerThrow){
-    console.log("Hai perso")
-} else {
-    console.log("Hai vinto!");
-}
+    // Throw dices
+    let userThrow = 1 + parseInt(Math.random() * 5);
+    let computerThrow = 1 + parseInt(Math.random() * 5);
+
+    // Log throws
+    console.log(`Hai lanciato ${userThrow}, il computer ha lanciato ${computerThrow}`);
+    throwComp.innerHTML = computerThrow;
+    throwUser.innerHTML = userThrow;
+
+    // Check for result
+    let result;
+    if (userThrow == computerThrow) {
+        result = "Pareggio";
+    } else if (userThrow < computerThrow) {
+        result = "Hai perso";
+    } else {
+        result = "Hai vinto!";
+    }
+
+    // Display result
+    console.log(result);
+    output.innerHTML = result;
+
+});
